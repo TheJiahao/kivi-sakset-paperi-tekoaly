57820eac88ecccd740073d295127f345a04869e9
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-03-22 14:59:40 +0200

    Toteuta pelialusta

---
+++ src/entities/peli.py
@@ -0,0 +1,66 @@
+class Peli:
+    """Luokka, joka kuvaa kivi-sakset-paperi-peliä.
+    """
+    
+    def __init__(self, syotteet: dict[str, int] | None = None) -> None:
+        """Luokan konstruktori.
+
+        Args:
+            syotteet (dict[str, int] | None, optional):
+                Vapaaehtoinen, oletusarvoltaan None.
+                Kuvaa merkkijonosyötteitä vastaavia lukuja.
+        """
+        self.__syotteet: dict[str, int] = syotteet or {"k": 0, "s": -1, "p": 1}
+
+    def paata_voittaja(self, pelaaja1: str, pelaaja2: str) -> int:
+        """Päättää kivi-sakset-paperi-pelin voittajan.
+
+        Args:
+            pelaaja1 (str): Pelaajan 1 syöte.
+            pelaaja2 (str): Pelaajan 2 syöte.
+
+        Returns:
+            int: Palauttaa -1, 0 tai 1; mitkä vastaavat pelaajan 1 häviötä, tasapelia tai voittoa.
+        """
+        tulos = 0
+
+        if pelaaja1 != pelaaja2:
+            p1 = self.__muunna_luvuiksi(pelaaja1)
+            p2 = self.__muunna_luvuiksi(pelaaja2)
+
+            match p1:
+                case -1:
+                    if p1 * p2 == -1:
+                        tulos = 1
+                    else:
+                        tulos = -1
+                case 0:
+                    if p1 > p2:
+                        tulos = 1
+                    else:
+                        tulos = -1
+                case 1:
+                    if p1 * p2 != -1:
+                        tulos = 1
+                    else:
+                        tulos = -1
+
+        return tulos
+
+    def __muunna_luvuiksi(self, merkki: str) -> int:
+        """Muuttaa merkin vastaavaksi luvuksi
+
+        Args:
+            merkki (str): Merkkijono, joka kuvaa pelaajan syötettä.
+
+        Raises:
+            ValueError: Merkkijono ei kelpaa.
+
+        Returns:
+            int: Syötettä vastaava luku.
+        """
+
+        if merkki not in self.__syotteet:
+            raise ValueError("Virhellinen merkki")
+
+        return self.__syotteet[merkki]
---
+++ src/tests/entities/test_peli.py
@@ -0,0 +1,22 @@
+import unittest
+from entities.peli import Peli
+
+
+class TestPeli(unittest.TestCase):
+    def setUp(self) -> None:
+        self.peli = Peli()
+
+    def test_voittotapaukset_oikein(self):
+        self.assertEqual(self.peli.paata_voittaja("k", "s"), 1)
+        self.assertEqual(self.peli.paata_voittaja("s", "p"), 1)
+        self.assertEqual(self.peli.paata_voittaja("p", "k"), 1)
+
+    def test_tasapelit_oikein(self):
+        self.assertEqual(self.peli.paata_voittaja("k", "k"), 0)
+        self.assertEqual(self.peli.paata_voittaja("s", "s"), 0)
+        self.assertEqual(self.peli.paata_voittaja("p", "p"), 0)
+
+    def test_haviot_oikein(self):
+        self.assertEqual(self.peli.paata_voittaja("k", "p"), -1)
+        self.assertEqual(self.peli.paata_voittaja("s", "k"), -1)
+        self.assertEqual(self.peli.paata_voittaja("p", "s"), -1)
