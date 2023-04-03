dd634ed60785e62913efc8372c9684cbb786c50d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-04-03 16:27:52 +0300

    Lisää testejä

--- src/tests/entities/tekoalyt/test_yhdistelma_tekoaly.py
+++ src/tests/entities/tekoalyt/test_yhdistelma_tekoaly.py
@@ -1,12 +1,22 @@
 import unittest
 
 from entities.peli import Peli
+from entities.tekoalyt.tekoaly import Tekoaly
 from entities.tekoalyt.yhdistelma_tekoaly import YhdistelmaTekoaly
 
 
 class TestYhdistelmaTekoaly(unittest.TestCase):
     def setUp(self) -> None:
         self.tekoaly = YhdistelmaTekoaly(3, Peli())
+
+    def hae_tekoalyn_pisteet(self, haettava: Tekoaly) -> int:
+        pisteet = 0
+
+        for tekoaly, pistetilanne in self.tekoaly.hae_tekoalyt_ja_pisteet():
+            if tekoaly == haettava:
+                pisteet = sum(pistetilanne)
+
+        return pisteet
 
     def test_ei_positiivinen_fokus_pituus_aiheuttaa_virheen(self):
         with self.assertRaises(ValueError):
@@ -22,6 +32,25 @@ class TestYhdistelmaTekoaly(unittest.TestCase):
         for pari in self.tekoaly.hae_tekoalyt_ja_pisteet():
             self.assertEqual(pari[1], tuple())
 
+    def test_hae_paras_tekoaly_antaa_parhaimman_tekoalyn(self):
+        paras = self.tekoaly.hae_paras_tekoaly()
+        parhaimman_pisteet = self.hae_tekoalyn_pisteet(paras)
+
+        for pari in self.tekoaly.hae_tekoalyt_ja_pisteet():
+            pisteet = pari[1]
+            self.assertGreaterEqual(parhaimman_pisteet, sum(pisteet))
+
+        self.tekoaly.lisaa("k")
+        self.tekoaly.lisaa("s")
+        self.tekoaly.lisaa("p")
+
+        paras = self.tekoaly.hae_paras_tekoaly()
+        parhaimman_pisteet = self.hae_tekoalyn_pisteet(paras)
+
+        for pari in self.tekoaly.hae_tekoalyt_ja_pisteet():
+            pisteet = pari[1]
+            self.assertGreaterEqual(parhaimman_pisteet, sum(pisteet))
+
     def test_lisaa_pienentaa_jaljella_olevia_siirtoja_yhdella(self):
         self.tekoaly.lisaa("k")
 
@@ -34,4 +63,7 @@ class TestYhdistelmaTekoaly(unittest.TestCase):
         self.tekoaly.lisaa("k")
         self.tekoaly.lisaa("k")
 
-        self.assertEqual(self.tekoaly.pelaava_tekoaly, self.tekoaly.hae_paras_tekoaly())
+        self.assertEqual(
+            self.hae_tekoalyn_pisteet(self.tekoaly.pelaava_tekoaly),
+            self.hae_tekoalyn_pisteet(self.tekoaly.hae_paras_tekoaly()),
+        )
